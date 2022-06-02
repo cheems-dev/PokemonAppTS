e069d574d99d68c1bd383505ad14cb2f3ebea39c
cheems-dev <ydaprimovaria@gmail.com>
2022-06-01 19:45:52 -0500

    ✨ feature: Create layout

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,22 @@
+import { FC, PropsWithChildren } from 'react';
+
+import Head from 'next/head';
+
+interface Props {
+  children: JSX.Element;
+  title?: string;
+}
+export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || 'Pokemon App'}</title>
+        <meta name='Author' content='Luis Ccalluchi Lopez' />
+        <meta name='description' content={`Más informacion sobre ${title}`} />
+        <meta name='keywords' content={`${title}, pokedex, pokemon `} />
+      </Head>
+      {/* Navbar */}
+      <main>{children}</main>
+    </>
+  );
+};
