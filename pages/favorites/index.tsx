ce0e781f465c7b434b671b3506e5434c39b8aa27
cheems-dev <ydaprimovaria@gmail.com>
2022-07-04 22:10:17 -0500

    🚧 feature: add pages

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../../components/layouts';
+import NextLink from 'next/link';
+import { Link } from '@nextui-org/react';
+
+export default function FavoritesPage() {
+  return (
+    <Layout title='Pokémons - Favoritos'>
+      <h1>Favoritos</h1>
+    </Layout>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
 import type { NextPage } from 'next';
 
-import { Button } from '@nextui-org/react';
-
 import { Layout } from '../components/layouts';
 
-const HomePage: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = (props) => {
+  const { pokemons } = props;
+
   return (
     <Layout title='Listado de Pokemons'>
-      <Button color={'gradient'}>Click me</Button>
+      <Grid.Container gap={2} justify='flex-start'>
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </Grid.Container>
     </Layout>
   );
+};
+
+import { GetStaticProps } from 'next';
+import { PokeApi } from '../api';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+import Image from 'next/image';
+import { PokemonCard } from '../components/pokemon/PokemonCard';
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await PokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, index) => ({
+    ...pokemon,
+    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      index + 1
+    }.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
 };
 
 export default HomePage;
